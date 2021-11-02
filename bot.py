75f07fcb44c18fb876f6454efb150c7595acc8e9
unknown <luckystripes60@gmail.com>
2021-11-02 03:42:11 -0700

    attempt to reduce runtime

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # bot.py
 import discord
-import random
 import json
 import os
 from dotenv import load_dotenv
@@ -11,15 +10,26 @@ client = discord.Client(intents=intents)
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-@client.event
+#used to create initial dm with users
 async def direct_message(person, message):
     member = discord.utils.get(message.guild.members, name=person)
     channel = await member.create_dm()
-    await channel.send("Send this bot art links that you want to post to other servers")
+    await channel.send("Send this bot art links that you want to post to other servers by using the command !send <link>")
+
+#recursive function to try and reduce runtime
+async def findChannel(channels, index):
+    found = -1
+    if index >= len(channels):
+        return found
+    if 'art' in channels[index].name:
+        found = index
+    else:
+        found = await findChannel(channels, index+1)
+    return found
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
@@ -27,6 +37,7 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    #test thing for ease of use 
     elif message.content == '!quit' or message.content == '!stop' or message.content == '!exit':
         print("quitting out")
         await client.logout()
@@ -35,14 +46,12 @@ async def on_message(message):
         await direct_message(message.author.name, message)
 
     elif message.content[:6] == '!send ' and message.guild == None:
-        for guild in client.guilds:
-            if guild.get_member(message.author.id) is not None:
-                for i in range(len(guild.text_channels)):
-                    try:
-                        await guild.text_channels[i].send(message.content[6:])
-                        break
-                    except:
-                        continue 
-        await message.channel.send("message sent") 
+        for guild in message.author.mutual_guilds:
+            index = await findChannel(guild.text_channels, 0) #find indexes of art channels
+            if index != -1:
+                await guild.text_channels[index].send(message.content[6:])
+            else:
+                await message.channel.send("Server " f"{guild} does not have an art channel. Please contact the dev if you actually see this") 
+        await message.channel.send("art sent") 
 
 client.run(TOKEN)
